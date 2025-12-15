4b4552aaf476e644dee071a518e189d57a2b6075
jenner-tech <jenner4022k@gmail.com>
2025-12-15 09:41:59 -0500

    Agrego cambios

--- Phyton1.1/trabajo1.py
+++ Phyton1.1/trabajo1.py
@@ -2,3 +2,25 @@ archivo = open("datos.txt","r", encoding="utf-8")
 contenido = archivo.read()
 print(contenido)
 archivo.close()
+
+#procesos crear archivo modo w - sobreescribe el archivo
+with open("Salida.txt","w",encoding="utf-8") as archivo:
+    archivo.write("Bienvenidos al Sena") 
+    archivo.write("\n") #salto de linea
+    archivo.write("Hola a todos  desde el CTPI - CAUCA")
+    archivo.write("\n")
+    archivo.write("Felicitación a todos....")
+    archivo.close()
+    
+
+#lectura de archivo usando try except y el metodo readlines
+
+try:
+    archivo = open ("salida.txt","r", encoding="utf-8")
+    lineas = archivo.readlines()
+    print(type(lineas))
+    print(lineas)
+except IOError as error:
+    print("Archivo no existe")
+    
+
